14be717ba5594485a249348b0b7e1fd5ebdd2e8e
Justin Su <justsu@umich.edu>
2025-01-30 18:23:22 -0500

    My changes before pulling latest updates

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/tisyam/Documents/eecs485/485p2/env
+set -gx VIRTUAL_ENV /Users/justinsu/Desktop/EECS485/p2/485p2/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH

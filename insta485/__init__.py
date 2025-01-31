f68e0d3f0477037d4f957081e19a4decc977c71c
anuragkrosuru <anuragkrosuru@gmail.com>
2025-01-30 20:28:10 -0500

    followers + following, need tp test

--- insta485/__init__.py
+++ insta485/__init__.py
@@ -1,7 +1,6 @@
 """Insta485 package initializer."""
 import flask
 
-
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 
---
+++ insta485/views/followers.py
@@ -0,0 +1,63 @@
+"""
+Insta485 followers view.
+
+URLs include:
+/users/<user_url_slug>/followers/
+"""
+import flask
+import insta485
+
+
+@insta485.app.route('/users/<user_url_slug>/followers/')
+def show_followers(user_url_slug):
+    """Display /users/<user_url_slug>/followers/ route."""
+    # Connect to database
+    connection = insta485.model.get_db()
+
+    # Check if user is logged in
+    if 'username' not in flask.session:
+        return flask.redirect(flask.url_for('show_login'))
+    
+    logname = flask.session['username']
+
+    # Check if the requested user exists
+    cur = connection.execute(
+        "SELECT username FROM users WHERE username = ?",
+        (user_url_slug,)
+    )
+    if not cur.fetchone():
+        flask.abort(404)
+
+    # Get list of followers for user_url_slug
+    cur = connection.execute(
+        """
+        SELECT 
+            u.username,
+            u.filename AS user_img_url,
+            EXISTS(
+                SELECT 1 
+                FROM following f2 
+                WHERE f2.username1 = ? AND f2.username2 = u.username
+            ) AS logname_follows_username
+        FROM following f
+        JOIN users u ON f.username1 = u.username
+        WHERE f.username2 = ?
+        ORDER BY u.username
+        """,
+        (logname, user_url_slug)
+    )
+    followers = cur.fetchall()
+
+    # Add URL prefix to user images
+    for follower in followers:
+        follower['user_img_url'] = flask.url_for(
+            'show_file', 
+            filename=follower['user_img_url']
+        )
+
+    # Render template
+    return flask.render_template(
+        "followers.html",
+        logname=logname,
+        followers=followers
+    )
